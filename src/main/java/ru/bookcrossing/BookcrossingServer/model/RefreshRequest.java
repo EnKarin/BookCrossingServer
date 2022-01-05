d685f086e66567687cf438874eb7ade16d23d1d9
Елагина Карина Юрьевна <karina.elagina2013@yandex.ru>
2022-01-05 19:18:45 +0700

    Добавлена документация для обновления токена

--- src/main/java/ru/bookcrossing/BookcrossingServer/model/RefreshRequest.java
+++ src/main/java/ru/bookcrossing/BookcrossingServer/model/RefreshRequest.java
@@ -1,9 +1,11 @@
 package ru.bookcrossing.BookcrossingServer.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class RefreshRequest {
 
+    @Schema(description = "Токен обновления", example = "cac2ce3e-9ff0-49a7-8afc-3dcae34eafea")
     private String refresh;
 }
