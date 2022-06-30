055170183452e7bc4a665a41e6a46bb6d04ecf7a
Елагина Карина Юрьевна <karina.elagina2013@yandex.ru>
2022-06-30 16:10:16 +0700

    Changed error codes, added ExceptionHandler, dto immutable

--- src/main/java/io/github/enkarin/bookcrossing/configuration/MailConfig.java
+++ src/main/java/io/github/enkarin/bookcrossing/configuration/MailConfig.java
@@ -1,4 +1,4 @@
-package io.github.enkarin.bookcrossing.mail.config;
+package io.github.enkarin.bookcrossing.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/io/github/enkarin/bookcrossing/user/service/ResetPasswordService.java
+++ src/main/java/io/github/enkarin/bookcrossing/user/service/ResetPasswordService.java
@@ -1,6 +1,6 @@
 package io.github.enkarin.bookcrossing.user.service;
 
-import io.github.enkarin.bookcrossing.errors.ErrorListResponse;
+import io.github.enkarin.bookcrossing.exception.TokenInvalidException;
 import io.github.enkarin.bookcrossing.mail.model.ActionMailUser;
 import io.github.enkarin.bookcrossing.mail.repository.ActionMailUserRepository;
 import io.github.enkarin.bookcrossing.user.dto.UserPasswordDto;
@@ -9,34 +9,23 @@ import io.github.enkarin.bookcrossing.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ResetPasswordService {
 
     private final ActionMailUserRepository actionMailUserRepository;
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Optional<ErrorListResponse> updatePassword(final String token, final UserPasswordDto passwordDto) {
-        final ErrorListResponse response = new ErrorListResponse();
-        User user;
-        if (!passwordDto.getPassword().equals(passwordDto.getPasswordConfirm())) {
-            response.getErrors().add("password: Пароли не совпадают");
-            return Optional.of(response);
-        }
-        final Optional<ActionMailUser> actionMailUser = actionMailUserRepository.findById(token);
-        if (actionMailUser.isPresent()) {
-            user = actionMailUser.get().getUser();
-            user.setPassword(bCryptPasswordEncoder.encode(passwordDto.getPassword()));
-            userRepository.save(user);
-            actionMailUserRepository.delete(actionMailUser.get());
-            return Optional.empty();
-        } else {
-            response.getErrors().add("token: Токен некорректен");
-            return Optional.of(response);
-        }
+    public void updatePassword(final String token, final UserPasswordDto passwordDto) {
+        final ActionMailUser actionMailUser = actionMailUserRepository.findById(token)
+                .orElseThrow(TokenInvalidException::new);
+        final User user = actionMailUser.getUser();
+        user.setPassword(bCryptPasswordEncoder.encode(passwordDto.getPassword()));
+        userRepository.save(user);
+        actionMailUserRepository.delete(actionMailUser);
     }
 }
