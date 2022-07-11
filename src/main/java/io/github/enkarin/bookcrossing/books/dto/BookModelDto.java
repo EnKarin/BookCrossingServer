30d65b761ce93ca6dee3da78002543d2d61f76b7
Елагина Карина Юрьевна <karina.elagina2013@yandex.ru>
2022-07-11 15:14:50 +0700

    All fields are made private and final with the help @Value

--- src/main/java/io/github/enkarin/bookcrossing/books/dto/BookModelDto.java
+++ src/main/java/io/github/enkarin/bookcrossing/books/dto/BookModelDto.java
@@ -5,54 +5,39 @@ import io.github.enkarin.bookcrossing.books.model.Book;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.Value;
 import lombok.experimental.SuperBuilder;
 
 import javax.validation.constraints.NotBlank;
 
 @SuperBuilder
-@EqualsAndHashCode
+@Value
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Schema(description = "Полные данные книги")
 public class BookModelDto {
 
-    @Getter
     @Schema(description = "Идентификатор", example = "15")
-    private final int bookId;
+    int bookId;
 
-    @Getter
     @Schema(description = "Название", example = "Портрет Дориана Грея")
     @NotBlank(message = "title: Название должно содержать хотя бы один видимый символ")
-    private final String title;
+    String title;
 
-    @Getter
     @Schema(description = "Автор", example = "Оскар Уайльд")
     @NotBlank(message = "author: Поле \"автор\" должно содержать хотя бы один видимый символ")
-    private final String author;
+    String author;
 
-    @Getter
     @Schema(description = "Жанр", example = "Классическая проза")
-    private final String genre;
+    String genre;
 
-    @Getter
     @Schema(description = "Издательство", example = "АСТ")
-    private final String publishingHouse;
+    String publishingHouse;
 
-    @Getter
     @Schema(description = "Год издания", example = "2004")
-    private final int year;
+    int year;
 
     @Schema(description = "Вложение")
-    private final Attachment attachment;
-
-    public Attachment getAttachment() {
-        if (attachment != null) {
-            return new Attachment(attachment.getAttachId(), attachment.getBook(), attachment.getData(),
-                    attachment.getExpansion());
-        }
-        return null;
-    }
+    Attachment attachment;
 
     public static BookModelDto fromBook(final Book book) {
         return new BookModelDto(book.getBookId(), book.getTitle(), book.getAuthor(), book.getGenre(),
