0c516c421445578a68aea265632e3e57fb8a4524
Елагина Карина Юрьевна <karina.elagina2013@yandex.ru>
2022-07-12 16:57:48 +0700

    Added GreenMail

---
+++ src/test/java/io/github/enkarin/bookcrossing/init/GreenMailInitializer.java
@@ -0,0 +1,28 @@
+package io.github.enkarin.bookcrossing.init;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+public class GreenMailInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    private static final DockerImageName IMAGE = DockerImageName.parse("greenmail/standalone:1.6.9");
+    private static final GenericContainer<?> GREEN_MAIL_CONTAINER = new GenericContainer<>(IMAGE);
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        GREEN_MAIL_CONTAINER
+                .waitingFor(Wait.forLogMessage(".*Starting GreenMail standalone.*", 1))
+                .withEnv("GREENMAIL_OPTS", "-Dgreenmail.setup.test.smtp -Dgreenmail.hostname=0.0.0.0" +
+                        " -Dgreenmail.users=duke:springboot")
+                .withExposedPorts(3025)
+                .start();
+        TestPropertyValues.of(
+                "spring.mail.host=" + GREEN_MAIL_CONTAINER.getHost(),
+                "spring.mail.port=" + GREEN_MAIL_CONTAINER.getFirstMappedPort()
+        ).applyTo(applicationContext.getEnvironment());
+    }
+}
--- src/test/java/io/github/enkarin/bookcrossing/registation/controllers/RegistrationControllerTest.java
+++ src/test/java/io/github/enkarin/bookcrossing/registation/controllers/RegistrationControllerTest.java
@@ -1,15 +1,20 @@
 package io.github.enkarin.bookcrossing.registation.controllers;
 
 import io.github.enkarin.bookcrossing.base.BookCrossingBaseTests;
+import io.github.enkarin.bookcrossing.init.GreenMailInitializer;
 import io.github.enkarin.bookcrossing.registation.dto.AuthResponse;
 import io.github.enkarin.bookcrossing.support.TestDataProvider;
 import io.github.enkarin.bookcrossing.user.dto.UserDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+
+import javax.mail.MessagingException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ContextConfiguration(initializers = GreenMailInitializer.class)
 class RegistrationControllerTest extends BookCrossingBaseTests {
 
     @AfterEach
@@ -19,17 +24,24 @@ class RegistrationControllerTest extends BookCrossingBaseTests {
     }
 
     @Test
-    void registerUserTest() {
-        int userId = webClient.post()
+    void registerUserTest() throws MessagingException {
+        UserDto user = webClient.post()
                 .uri("/registration")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(TestDataProvider.buildBot())
                 .exchange()
                 .expectStatus().isEqualTo(201)
                 .expectBody(UserDto.class)
-                .returnResult().getResponseBody()
-                .getUserId();
-        usersId.add(userId);
+                .returnResult().getResponseBody();
+        assertThat(user).isNotNull();
+
+        usersId.add(user.getUserId());
+
+//        MimeMessage receivedMessage = greenMail.getReceivedMessages()[0];
+//        assertThat(receivedMessage.getAllRecipients())
+//                .hasSize(1)
+//                .extracting(Address::toString)
+//                .isEqualTo(List.of(user.getEmail()));
     }
 
     @Test
