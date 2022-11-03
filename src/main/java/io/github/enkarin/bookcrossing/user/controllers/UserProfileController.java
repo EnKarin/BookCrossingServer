3e27ad33fc651648b3998e8af026f6adf7ecd9db
lb416jt <lukas.basala@student.tuke.sk>
2022-11-03 12:52:56 +0100

    moving userId to header

--- src/main/java/io/github/enkarin/bookcrossing/user/controllers/UserProfileController.java
+++ src/main/java/io/github/enkarin/bookcrossing/user/controllers/UserProfileController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -57,7 +58,7 @@ public class UserProfileController {
         }
     )
     @GetMapping
-    public ResponseEntity<?> getProfile(@RequestParam(defaultValue = "-1") @Parameter(description = "Идентификатор пользователя") final int userId, // NOSONAR
+    public ResponseEntity<?> getProfile(@RequestHeader(defaultValue = "-1") @Parameter(description = "Идентификатор пользователя") final int userId, // NOSONAR
                                         @RequestParam @Parameter(description = "Часовой пояс пользователя") final int zone,
                                         final Principal principal) {
         if (userId == -1) {
