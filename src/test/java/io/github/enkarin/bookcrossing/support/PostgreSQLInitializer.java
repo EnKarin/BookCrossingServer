2c96702a3a22643a1e0f656f167bc8d8a6ff721e
i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>
2023-05-18 20:20:06 +0400

    Fixed error when running with colima

--- src/test/java/io/github/enkarin/bookcrossing/support/PostgreSQLInitializer.java
+++ src/test/java/io/github/enkarin/bookcrossing/support/PostgreSQLInitializer.java
@@ -4,20 +4,21 @@ import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 public class PostgreSQLInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
     private static final DockerImageName IMAGE = DockerImageName.parse("postgres:14.6-alpine");
-    private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(IMAGE);
+    private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(IMAGE)
+            .withUsername("root")
+            .withPassword("root")
+            .withDatabaseName("bookcrossing")
+            .waitingFor(Wait.forListeningPort());
 
     @Override
     public void initialize(final ConfigurableApplicationContext applicationContext) {
-        POSTGRE_SQL_CONTAINER
-                .withUsername("root")
-                .withPassword("root")
-                .withDatabaseName("bookcrossing")
-                .start();
+        POSTGRE_SQL_CONTAINER.start();
         TestPropertyValues.of(
                 "spring.datasource.url=" + POSTGRE_SQL_CONTAINER.getJdbcUrl(),
                 "spring.datasource.username=" + POSTGRE_SQL_CONTAINER.getUsername(),
