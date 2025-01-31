7baa439359ad7a6f5874bcd7231d08ea440553f4
Aleksey <alleksey-Kn@yandex.ru>
2025-01-31 23:22:06 +0700

    Fix cors policy

--- src/main/java/io/github/enkarin/bookcrossing/security/SecurityConfig.java
+++ src/main/java/io/github/enkarin/bookcrossing/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import io.github.enkarin.bookcrossing.handlers.AuthenticationEntryPointHandler;
 import io.github.enkarin.bookcrossing.security.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,23 +12,37 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     private final JwtFilter jwtFilter;
     private final AuthenticationEntryPointHandler handler;
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @SneakyThrows
     @Bean
     protected SecurityFilterChain filterChain(final HttpSecurity httpSecurity) {
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+
         httpSecurity
                 .requiresChannel().antMatchers().requiresSecure()
                 .and()
                 .httpBasic().disable()
                 .csrf().disable()
+                .cors().configurationSource(request -> corsConfiguration)
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
