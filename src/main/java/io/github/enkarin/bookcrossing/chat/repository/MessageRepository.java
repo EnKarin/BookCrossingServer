f8f2ea4308a2114b5983968acb5e9e6dd3e5350a
Aleksey <alleksey-Kn@yandex.ru>
2025-02-10 18:31:10 +0700

    Add MessageRepository.countAllUnreadMessageFromSpecifiedChatAndToCurrentUser

--- src/main/java/io/github/enkarin/bookcrossing/chat/repository/MessageRepository.java
+++ src/main/java/io/github/enkarin/bookcrossing/chat/repository/MessageRepository.java
@@ -1,7 +1,12 @@
 package io.github.enkarin.bookcrossing.chat.repository;
 
+import io.github.enkarin.bookcrossing.chat.model.Correspondence;
 import io.github.enkarin.bookcrossing.chat.model.Message;
+import io.github.enkarin.bookcrossing.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    @Query("SELECT COUNT(m) FROM Message m WHERE m.correspondence = ?1 AND m.sender != ?2 AND m.declaim = false")
+    int countAllUnreadMessageFromSpecifiedChatAndToCurrentUser(final Correspondence chat, final User user);
 }
