68f1de1fe005494f3cc0a9bbc87d0cda4044fbbc
Aleksey <alleksey-Kn@yandex.ru>
2025-02-18 00:15:29 +0700

    Add AutocompletionService and AutocompletionController

---
+++ src/main/java/io/github/enkarin/bookcrossing/books/controllers/AutocompletionController.java
@@ -0,0 +1,12 @@
+package io.github.enkarin.bookcrossing.books.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/books/autocompletion")
+public class AutocompletionController {
+
+}
--- src/main/java/io/github/enkarin/bookcrossing/books/repository/BookRepository.java
+++ src/main/java/io/github/enkarin/bookcrossing/books/repository/BookRepository.java
@@ -2,6 +2,7 @@ package io.github.enkarin.bookcrossing.books.repository;
 
 import io.github.enkarin.bookcrossing.books.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,10 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
     Optional<Book> findBooksByOwnerLoginAndBookId(String login, int id);
 
     List<Book> findBooksByOwnerUserId(int userId);
+
+    @Query(value = "SELECT * FROM bookcrossing.t_book WHERE lower(title) LIKE lower(concat('%', ?1,'%')) LIMIT 5", nativeQuery = true)
+    List<Book> findBooksByPartOfName(String partName);
+
+    @Query(value = "SELECT * FROM bookcrossing.t_book WHERE author IS NOT NULL AND lower(author) LIKE lower(concat('%', ?1,'%')) LIMIT 5", nativeQuery = true)
+    List<Book> findBooksByPartOfAuthor(String partName);
 }
