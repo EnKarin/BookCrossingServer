7f7529e7850e42f5d16f30d488503e6feeb49f83
flowihs <zmulenko500@gmail.com>
2025-03-17 21:48:37 +0300

    Update book status enum

--- src/main/java/io/github/enkarin/bookcrossing/books/enums/Status.java
+++ src/main/java/io/github/enkarin/bookcrossing/books/enums/Status.java
@@ -2,7 +2,7 @@ package io.github.enkarin.bookcrossing.books.enums;
 
 //@Enumerated(EnumType.STRING)
 public enum Status {
-    GIVE_RU ("Отдает"), GIVE_ENG("Gives away"), EXCHANGES_RU("Отдает"), EXCHANGES_ENG("Exchanges");
+    GIVE_RU("Отдает"), GIVE_ENG("Gives away"), EXCHANGES_RU("Отдает"), EXCHANGES_ENG("Exchanges");
 
     Status(String s) {
     }
