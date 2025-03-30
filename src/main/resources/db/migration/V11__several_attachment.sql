77a90167f31fcd89f909330e13a26b9368a2150a
Aleksey <alleksey-Kn@yandex.ru>
2025-03-31 01:25:53 +0700

    Add V11__several_attachment.sql

---
+++ src/main/resources/db/migration/V11__several_attachment.sql
@@ -0,0 +1,10 @@
+alter table bookcrossing.t_attach add column book_id integer not null;
+ALTER TABLE bookcrossing.t_attach DROP CONSTRAINT attach_foreign_book;
+alter table bookcrossing.t_attach
+    add constraint attach_foreign_book
+        foreign key (book_id) references bookcrossing.t_book (book_id);
+
+alter table bookcrossing.t_book add column title_attachment integer not null unique;
+alter table bookcrossing.t_book
+    add constraint book_foreign_title_attachment
+        foreign key (title_attachment) references bookcrossing.t_attach (attach_id);
