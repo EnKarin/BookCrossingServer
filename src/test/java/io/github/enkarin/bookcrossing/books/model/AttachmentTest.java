4acc4aaaa4f38de4b0136fb72079b0e6e30d2658
Ivan Vakhrushev <mfvanek@gmail.com>
2025-08-17 13:23:49 +0400

    Fix coverage degradation

---
+++ src/test/java/io/github/enkarin/bookcrossing/books/model/AttachmentTest.java
@@ -0,0 +1,28 @@
+package io.github.enkarin.bookcrossing.books.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AttachmentTest {
+
+    @Test
+    void equalsAndHashCodeShouldWork() {
+        final Attachment first = new Attachment();
+        final Attachment second = new Attachment();
+
+        //noinspection AssertBetweenInconvertibleTypes
+        assertThat(first)
+                .isEqualTo(first)
+                .isNotEqualTo(new Book());
+
+        assertThat(first)
+                .isEqualTo(second)
+                .hasSameHashCodeAs(second);
+
+        first.setAttachId(11);
+        assertThat(first)
+                .isNotEqualTo(second)
+                .hasSameHashCodeAs(second);
+    }
+}
